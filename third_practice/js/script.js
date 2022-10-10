d455f45b54cc114ec187aff509ce2d84f8e4eca4
Artem Ivanitskiy <nokerzekul@gmail.com>
2022-10-10 17:35:26 +0400

    Add trim

--- third_practice/js/script.js
+++ third_practice/js/script.js
@@ -35,8 +35,8 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+              b = prompt('На сколько оцените его?', '').trim();
          
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
@@ -74,8 +74,7 @@ showMyDB(personalMovieDB.privat);
 
 function writeYourGenres() {
     for (let i = 1; i <= 3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.genres[i - 1] = genre;
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
     }
 }
 
